402458db210ee39bd4958fa31b90fbe1757d3fd5
Mir Mujtaba Habib <i210661@nu.edu.pk>
2026-05-26 01:40:59 +0500

    Updated Fee structure

--- src/pages/StudentAdmission.tsx
+++ src/pages/StudentAdmission.tsx
@@ -22,9 +22,9 @@ const emptyStudent: Omit<Student, 'id'> = {
 };
 
 const FEE_STRUCTURE = [
-  { label: 'Nursery', fee: 1400 }, { label: 'KG', fee: 1300 }, { label: 'Prep', fee: 1400 },
-  { label: 'Class 1 - 2', fee: 1500 }, { label: 'Class 3 - 4', fee: 1600 },
-  { label: 'Class 5 - 6', fee: 1700 }, { label: 'Class 7', fee: 1800 }, { label: 'Class 8', fee: 2000 },
+  { label: 'PG', fee: 1300 }, { label: 'Nursery', fee: 1400 }, { label: 'Prep', fee: 1500 },
+  { label: 'Class 1 - 2', fee: 1600 }, { label: 'Class 3 - 4', fee: 1700 },
+  { label: 'Class 5 - 6', fee: 1800 }, { label: 'Class 7', fee: 1900 }, { label: 'Class 8', fee: 2000 },
   { label: 'Class 9', fee: 2500 }, { label: 'Class 10', fee: 2700 },
 ];
 
